1b52cba3da205d459d7d9827a9923ca420a12d73
dindss <145428056+adindaaftmh@users.noreply.github.com>
2024-08-13 09:03:55 +0700

    Mengontrol aliran program (if, If.. Else)

---
+++ Bab 6/bab6soal1/src/main/java/com/mycompany/bab6soal1/Bab6soal1.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab6soal1;
+import java.util.Scanner;
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab6soal1 {
+
+    public static void main(String[] args) {
+           // TODO code application logic here
+        Scanner scanner = new Scanner (System.in);
+         
+        System.out.print("NIM ? : ");
+        String nim = scanner.nextLine();
+        
+        System.out.print("NAMA ? : ");
+        String nama = scanner.nextLine();
+        
+        System.out.print("NILAI ? : ");
+        int nilai = scanner.nextInt();
+         
+        String grade;
+        if (nilai >= 80 ) {
+            grade = "A";
+        } else if (nilai >= 70)  {
+            grade = "B";
+        } else if (nilai >= 60) {
+            grade = "c";
+        } else if (nilai >= 50) {
+            grade = "D";
+        } else {
+            grade = "E";
+
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+        
+        }
+
+    /**
+     * @param args the command line arguments
+     */   
+    }
+}
+
