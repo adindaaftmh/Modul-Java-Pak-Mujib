661e35f3138fcd3f35f427cd6f72e915ea5d6ada
dindss <145428056+adindaaftmh@users.noreply.github.com>
2024-09-10 11:13:36 +0700

    Add files via upload

---
+++ bab13/bab13soal2/src/main/java/com/mycompany/bab13soal2/Bab13soal2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab13soal2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab13soal2 {
+
+    public static void main(String[] args) {
+        SegitigaCalculator objek = new SegitigaCalculator();
+        
+        double luas1 = objek.hitungluas(15, 20);
+        System.out.println("Luas segitiga dengan alas 15 dan tinggi 20 adalah : " + luas1);
+    }
+}
---
+++ bab13/bab13soal2/src/main/java/com/mycompany/bab13soal2/SegitigaCalculator.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.bab13soal2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class SegitigaCalculator {
+    double hitungluas(double alas, double tinggi) {
+        return (alas * tinggi) / 2;
+    }
+    
+    double hitungluas(double alas, double tinggi,double luas) {
+        return (alas * tinggi) / 2;
+    }
+}
