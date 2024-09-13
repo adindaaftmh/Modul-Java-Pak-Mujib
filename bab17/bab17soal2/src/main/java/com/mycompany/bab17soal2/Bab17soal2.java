9cf133d71ba88e3abd7e9d9238860392ddde89e5
dindss <145428056+adindaaftmh@users.noreply.github.com>
2024-09-13 08:03:47 +0700

    Add files via upload

---
+++ bab17/bab17soal2/src/main/java/com/mycompany/bab17soal2/Bab17soal2.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bab17soal2;
+
+/**
+ *
+ * @author PC RPL - R1
+ */
+public class Bab17soal2 {
+
+   public static void main(String[] args) {
+        // Membuat array multidimensi untuk menyimpan nama negara dan ibu kotanya
+        String[][] negaraDanIbukota = {
+            {"Indonesia", "Jakarta"},
+            {"Jepang", "Tokyo"},
+            {"Iran", "Teheran"},
+            {"Malaysia", "Kuala Lumpur"},
+            {"Thailand", "Bangkok"}
+        };
+
+        // Mengakses array dan menampilkan nama negara beserta ibukotanya
+        for (String[] negaraDanIbukota1 : negaraDanIbukota) {
+            System.out.println("Ibukota " + negaraDanIbukota1[0] + " adalah " + negaraDanIbukota1[1]);
+        }
+    }
+}
